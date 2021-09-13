c46fbb8c36a0c674dc41a0de08b61943dc2529c6
Szabó Gábor <91szabogabor@gmail.com>
2021-09-13 11:18:51 +0200

    third commit

--- src/Input.js
+++ src/Input.js
@@ -1,16 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Input = () => {
   const [amount, setAmount] = useState();
   const [rate, setRate] = useState([]);
 
-    const getData = async () => {
-    const response = await axios.get(
-      'https://jsonplaceholder.typicode.com/posts'
-    );
-    setRate(response.data);
-    console.log(userData);
+  const getRate = async () => {
+    try {
+      const response = await axios.get(
+        'https://api.exchangerate.host/latest?base=huf'
+      );
+      setRate(response.data);
+      console.log(rate);
+    } catch {
+      console.error('Nem sikerült a lekérés.');
+    }
   };
+
+  useEffect(() => {
+    getRate();
+  }, []);
 
   return (
     <div className='form-group'>
@@ -18,6 +27,11 @@ const Input = () => {
       <div>Forint (HUF)</div>
       <input className='form-control' type='number' value={amount} onChange={(e) => setAmount(e.target.value)} />
       <div>{amount}</div>
+      {rate.map(el => {
+        return (
+          <div>{el.USD}</div>
+        );
+      })}
     </div>
   );
 };
